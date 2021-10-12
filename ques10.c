c8554988b689cb0ecf2f81b2c1d16618a3bbaf5a
Shruti Sharan <74863869+sharanshruti@users.noreply.github.com>
2021-10-12 17:34:10 +0530

    Add files via upload

--- ques10.c
+++ ques10.c
@@ -1,13 +1,15 @@
 #include<stdio.h>
+
 int main()
 {
-	char ch=2;
-	if ((ch>=97 && ch<=122)|| (ch>=65 && ch<=90)){
-		printf("the enterd character is an alphabet. %c\n",ch);
-	}else if (ch>=0 && ch<=9){
-		printf("the enterd character is a digit. %c\n",ch);
-	}else{
-		printf("the entered character is something else.\n");
+	int n= 1234;
+	int digits= 0;
+	while(n)
+	{
+		n=n/10;
+		digits++;
 	}
+
+	printf("The number of digits is %d\n",digits);
 	return 0;
 }
--- ques12.c
+++ ques12.c
@@ -1,11 +1,12 @@
 #include<stdio.h>
 int main()
 {
-	double a=45, b=67,c=180-a-b;
-	if (a+b+c==180 && a>0 && b>0 && c>0){
-		printf("The triangle is valid.\n");
-	}else{
-		printf("The triangle is invalid.\n");
+	int i;
+	for(i=1;i<=10;i++)
+	{
+		int pro=8*i;
+		printf("8*%d= %d\n",i,pro);
 	}
+
 	return 0;
 }
